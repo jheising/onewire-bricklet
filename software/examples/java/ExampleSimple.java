46847cac380151bbc54653bbbd119a270bab33ef
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 14:41:00 +0100

    Make Java examples Protocol 2.0 compatible

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -7,15 +7,13 @@ public class ExampleSimple {
 	private static final String UID = "ABC"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickletTemperature temp = new BrickletTemperature(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletTemperature temp = new BrickletTemperature(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(temp); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Get current temperature (unit is °C/100)
 		short temperature = temp.getTemperature(); // Can throw IPConnection.TimeoutException
@@ -23,6 +21,5 @@ public class ExampleSimple {
 		System.out.println("Temperature: " + temperature/100.0 + " °C");
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
